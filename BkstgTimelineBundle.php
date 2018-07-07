84c075458450c818bedf0ad32800b3de44e543ff
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-07-06 21:08:54 -0400

    Workaround for symfony 4.

--- BkstgTimelineBundle.php
+++ BkstgTimelineBundle.php
@@ -2,8 +2,10 @@
 
 namespace Bkstg\TimelineBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BkstgTimelineBundle extends Bundle
 {
+    const TRANSLATION_DOMAIN = 'BkstgTimelineBundle';
 }
