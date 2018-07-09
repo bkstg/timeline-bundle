4e574cfb2360ff4f28c637ce165ab332fdbd48dd
Luke Bainbridge <luke.bainbridge@gmail.com>
2018-07-09 12:56:29 -0400

    Timelines are good now.

--- Block/ProductionTimelineBlock.php
+++ Block/ProductionTimelineBlock.php
@@ -16,6 +16,7 @@ use Sonata\BlockBundle\Block\Service\AbstractBlockService;
 use Sonata\BlockBundle\Templating\TwigEngine;
 use Spy\Timeline\Driver\ActionManagerInterface;
 use Spy\Timeline\Driver\TimelineManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
@@ -25,27 +26,35 @@ class ProductionTimelineBlock extends AbstractBlockService
     private $action_manager;
     private $timeline_manager;
     private $token_storage;
+    private $request_stack;
 
     public function __construct(
         $name,
         TwigEngine $templating,
         ActionManagerInterface $action_manager,
         TimelineManagerInterface $timeline_manager,
-        TokenStorageInterface $token_storage
+        TokenStorageInterface $token_storage,
+        RequestStack $request_stack
     ) {
         $this->action_manager = $action_manager;
         $this->timeline_manager = $timeline_manager;
         $this->token_storage = $token_storage;
+        $this->request_stack = $request_stack;
         parent::__construct($name, $templating);
     }
 
     public function execute(BlockContextInterface $context, Response $response = null)
     {
+        $request = $this->request_stack->getCurrentRequest();
         $production = $context->getBlock()->getSetting('production');
         $production_component = $this->action_manager->findOrCreateComponent($production);
         $timeline = $this->timeline_manager->getTimeline(
             $production_component,
-            ['paginate' => false, 'max_per_page' => 20]
+            [
+                'page' => $request->query->getInt('page', 1),
+                'paginate' => true,
+                'max_per_page' => 25,
+            ]
         );
 
         return $this->renderResponse($context->getTemplate(), [
@@ -60,7 +69,7 @@ class ProductionTimelineBlock extends AbstractBlockService
     {
         $resolver->setDefaults([
             'production' => null,
-            'template' => '@BkstgTimeline/Block/production_timeline.html.twig',
+            'template' => '@BkstgTimeline/Block/_production_timeline.html.twig',
         ]);
     }
 }
